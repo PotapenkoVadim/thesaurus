738d88254330b0780b17ef1486ad39fbf515c573
PotapenkoVadim <potapenko.v00@mail.ru>
2025-03-30 18:02:42 +0300

    feat: notfound page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,6 +1,12 @@
+import { Link } from "react-router";
+import styles from './NotFoundPage.module.scss';
+
 const NotFoundPage = () => {
   return (
-    <div>Not Found</div>
+    <div className={styles['notfound']}>
+      <div className={styles['notfound__description']}>Такой страницы не существует.</div>
+      <Link className={styles['notfound__link']} to='/'>Вернуться на главную</Link>
+    </div>
   );
 };
 
