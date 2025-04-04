090c498d78d9e9044708c9fe867aa84dce2561ed
PotapenkoVadim <potapenko.v00@mail.ru>
2025-04-04 21:15:03 +0300

    feat: fetching word details

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from './useAnimate';
 export * from './useLockBodyScroll';
 export * from './useWords';
+export * from './useWordDetails';
--- src/pages/WordPage/WordPage.tsx
+++ src/pages/WordPage/WordPage.tsx
@@ -1,21 +1,33 @@
 import { useParams } from "react-router";
-import { SynonymsList, WordActions, WordContainer } from "../../components";
+import { Spinner, SynonymsList, WordActions, WordContainer } from "../../components";
 import styles from './WordPage.module.scss';
 
 // TODO: should remove
-import { word, synonymsList } from "../../mock";
+import { synonymsList } from "../../mock";
+import { useWordDetails } from "../../hooks";
+import { useEffect } from "react";
 
 const WordPage = () => {
   const {id} = useParams();
 
+  const {error, getWord, word} = useWordDetails();
+
   const onDelete = () => console.log('DELETE: ', id);
   const onEdit = () => console.log('EDIT: ', id);
+
+  useEffect(() => {
+    if (id) {
+      getWord(id);
+    }
+  }, [id]);
 
   return (
     <>
       <WordActions onDelete={onDelete} onEdit={onEdit} />
-      <WordContainer word={word} className={styles['word-page__container']} />
-      <SynonymsList synonymsList={synonymsList} className={styles['word-page__container']} />
+      {!word && !error && <Spinner className={styles['word-page__spinner']} />}
+      {error && <div className={styles['word-page__error']}>{error}</div>}
+      {word && <WordContainer word={word} className={styles['word-page__container']} />}
+      {synonymsList && <SynonymsList synonymsList={synonymsList} className={styles['word-page__container']} />}
     </>
   );
 };
